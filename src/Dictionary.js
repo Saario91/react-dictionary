c6f879d7ff63226a335d3b036c97d66a5130a686
LZugar <59911931+LZugar@users.noreply.github.com>
2022-05-02 17:35:24 +0200

    Added functional API for dictionary calls
    https://dictionaryapi.dev/ LInk to API documentation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,25 @@
 import React, {useState } from 'react';
+import axios from 'axios';
 import './Dictionary.css';
 
 export default function Dictionary(){
     const [keyword, setKeyword] = useState('');
+
+    function handleAPI(response){
+        console.log(response);
+    }
     function search(event){
         event.preventDefault();
         alert(keyword);
+        let APIurl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+        axios.get(APIurl).then(handleAPI);
     }
     function handleKeywordChange(event){
         setKeyword(event.target.value)
     }
+
+    
     return(<div className='Dictionary'>
         <form onSubmit={search}>
             <input type='search' autoFocus={true} onChange={handleKeywordChange}/>
